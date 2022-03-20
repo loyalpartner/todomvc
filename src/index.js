ea9f5c34d92ca8085f6523eb963e85c1b6f5d1e4
lee <loyalpartner@163.com>
2022-03-20 23:31:25 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import App from "./pages/Home";
 import reducer from "./reducers";
 import "todomvc-app-css/index.css";
+import thunk from "redux-thunk";
+import { createLogger } from "redux-logger";
 
-const store = createStore(reducer);
+const middleware = [thunk];
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(createLogger());
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware));
 
 render(
   <Provider store={store}>

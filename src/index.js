dad1243eb3ecbd42cd43d38b8fc750cdbbfc6c74
lee <loyalpartner@163.com>
2022-03-20 23:35:16 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
+import "todomvc-app-css/index.css";
 import App from "./pages/Home";
 import reducer from "./reducers";
-import "todomvc-app-css/index.css";
-import thunk from "redux-thunk";
-import { createLogger } from "redux-logger";
 
 const middleware = [thunk];
 if (process.env.NODE_ENV !== "production") {
